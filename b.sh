af1606531e8e0ba1e30c2e5ac83ee140e763996d
Marcus0086 <43565099+Marcus0086@users.noreply.github.com>
2018-09-25 12:23:24 +0530

    Add files via upload

---
+++ b.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dpkg -s $1 &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Package  is installed!"
+else
+    echo "Package  is NOT installed!"
+fi
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash 1.sh
+echo "$(rm -f setup.sh.x)"
+echo "$(rm -f setup.sh.x.c)"
+echo "DONE."
